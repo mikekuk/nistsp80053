c0b6128288195008e60f6b36929272d51e68c868
Mike <mikekearney85@hotmail.com>
2024-10-31 15:31:34 +0000

    removed print from debuging

--- baselines.py
+++ baselines.py
@@ -41,15 +41,12 @@ baseline_nist_sp_800_53_r4_low = Baseline(controls=low,  name='Low', revision=4)
 baseline_nist_sp_800_53_r4_moderate = Baseline(controls=moderate,  name='Moderate', revision=4)
 baseline_nist_sp_800_53_r4_high = Baseline(controls=high,  name='High', revision=4)
 
-print(low)
-
 del privacy
 del low
 del moderate
 del high
 
 privacy, low, moderate, high = extract_baselines(Nist_sp_800_53_r5())
-print(low)
 
 baseline_nist_sp_800_53_r5_privacy = Baseline(controls=privacy, name='Privacy', revision=5)
 baseline_nist_sp_800_53_r5_low = Baseline(controls=low,  name='Low', revision=5)
